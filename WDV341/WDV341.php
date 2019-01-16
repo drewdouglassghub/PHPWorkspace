397df9eb8d35513df698b823aeac50c554c98ea6
drewdouglassghub <44046707+drewdouglassghub@users.noreply.github.com>
2019-01-15 21:20:56 -0600

    small update saved
    update

---
+++ WDV341/WDV341.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf-8">
+<script src="modernizr-2.js"></script>	
+<!--
+ My WDV221 Homework
+ Author: Drew Douglass
+ Date: 1/11/19
+-->
+<title>PHP Homework</title>
+<link href="style.css" rel="stylesheet" style="text/css"/>
+</head>
+<body>
+
+	<header>
+	PHP
+	</header>
+
+	<nav>
+		<ul>
+			<li><a href="http://www.drewdouglass.net/index.html">Home</a><li>
+			<li><a href="http://drewdouglass.net/WDV341/testGit.php">TestGit</a><li>
+		</ul>
+	</nav>
+	
+	
+	
+</body>
+</html>
